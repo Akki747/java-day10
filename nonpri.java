40fb4f90682d394ce389bc427c5cc23cefd5d4e2
Akki747 <bairapuakshaya@gmail.com>
2024-12-11 13:40:45 +0530

    Add files via upload

---
+++ nonpri.java
@@ -0,0 +1,12 @@
+class nonpri
+{
+    public static String a()
+    {
+        return"hello students";
+    }
+    public static void main(String args[])
+    {
+        String res=a();
+        System.out.println(res);
+    }
+}
