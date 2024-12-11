2b0fc7d4dbc804c0eb437c034e9a4a07a9f4de22
Akki747 <bairapuakshaya@gmail.com>
2024-12-11 15:07:40 +0530

    Add files via upload

--- main.java
+++ main.java
@@ -1,16 +1,22 @@
-class main
-{
-    public static void main(String args[])
-    {
-        int a=0;
-        int b=1;
-        for(int i=2;i<=10;i++)
-        {
-            int c=a+b;
-            System.out.println(i+"->"+c);
-            a=b;
-            b=c;
-        }
+import java.util.*;
+class Constructor {
+    int c;
+    String b;
 
+    // Correct the constructor name to match the class name
+    Constructor() {
+        this.c = 345; // Use 'this' in lowercase
+        this.b = "Akshaya";
+    }
+
+    void display() {
+        System.out.println(b + " " + c);
+    }
+}
+
+public class main {
+    public static void main(String[] args) {
+        Constructor s = new Constructor();
+        s.display();
     }
 }
